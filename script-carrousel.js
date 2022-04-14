f483d99da9c17b8d73b5fa7cdf5d69739ca8c4fd
Zekki10 <ezequiel_vera_rodriguez@hotmail.com>
2022-04-13 22:45:51 -0300

    TP codo a codo 4.0

---
+++ script-carrousel.js
@@ -0,0 +1,45 @@
+'use strict'
+window.addEventListener("load", ()=>{
+
+    'use strict'
+
+    const contenedor    = document.querySelector('.carrousel_container')
+    const boton         = document.querySelectorAll('.selector_carrousel')
+    
+    // Cuando CLICK en boton
+        // Saber la posición de ese boton
+        // Aplicar un transform translateX al contenedor
+        // QUITAR la clase .activo de todos los botones
+        // AÑADIR la clase .activo al punto que hemos hecho Click
+    
+    // Recorrer TODOS los punto
+    boton.forEach( ( botonSeleccionado , i )=> {
+        // Asignamos un CLICK a cadaPunto
+        boton[i].addEventListener('click',()=>{
+    
+            // Guardar la posición de ese boton
+            let posicion  = i
+            // Calcula el espacio que debe desplazar el contenedor
+            let operacion = posicion * -33
+    
+
+            contenedor.style.transform = `translateX(${ operacion }%)`
+    
+            // Recorremos los botones
+            boton.forEach( ( cadaPunto , i )=>{
+                // Quitamos la clase .activo a todos los botones
+                boton[i].classList.remove('activo')
+            })
+            // Añadir la clase activo en el boton que hemos hecho CLICK
+            boton[i].classList.add('activo')
+    
+        })
+    })
+
+    // evento girar al hacer click //
+
+});
+
+
+
+
